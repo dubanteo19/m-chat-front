3a2435ebfd809f8b23f85814842cc99dd5c192bc
dubanteo19 <dubanteo2003@gmail.com>
2026-07-17 16:59:39 +0700

    intorduce hover card, popover

--- src/lib/components/chat/message/message-toolbar.svelte
+++ src/lib/components/chat/message/message-toolbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+	import * as Popover from '$lib/components/ui/popover/index.js';
 	import { ALL_EMOJIS } from '$lib/constants/emojis';
 	import type { Message } from '$lib/types/message';
 	import { Ellipsis, ReplyIcon } from '@lucide/svelte';
@@ -16,17 +17,15 @@
 
 	const visibleEmojis = ALL_EMOJIS.slice(0, 5);
 	const hiddenEmojis = ALL_EMOJIS.slice(5);
-	let showMore = $state(false);
 
 	function selectEmoji(emoji: string) {
 		sendReact?.(message.id, emoji);
 		setOpenReactionId(null);
-		showMore = false;
 	}
 </script>
 
 <div
-	class="absolute -top-6 z-20 flex items-center gap-1 bg-secondary color-accent border-primary border-1 shadow-2xs rounded-xl px-2 py-1 transition-all fade-in
+	class="absolute -top-6 z-20 flex items-center gap-1 bg-secondary color-accent border-primary border shadow-2xs rounded-xl px-2 py-1 transition-all fade-in
 	{message.isMine ? 'right-2' : 'left-2'}
 	{openReactionId === message.id ? 'flex' : 'hidden md:group-hover:flex'}"
 >
@@ -46,31 +45,29 @@
 			</Button>
 		{/each}
 
-		<Button
-			onclick={() => (showMore = !showMore)}
-			size="icon"
-			variant="link"
-			title="More reactions"
-		>
-			<Ellipsis />
-		</Button>
-
-		{#if showMore}
-			<div
-				class="absolute bottom-full left-0 mb-1 z-50 grid grid-cols-6 gap-1.5 p-1.5 bg-white rounded-lg shadow-lg animate-in fade-in slide-in-from-bottom-2"
-			>
-				{#each hiddenEmojis as emoji (emoji)}
-					<Button
-						onclick={() => selectEmoji(emoji)}
-						variant="link"
-						class="hover:scale-125 active:scale-90  rounded-full"
-						title="React with {emoji}"
-					>
-						{emoji}
-					</Button>
-				{/each}
-			</div>
-		{/if}
+		<Popover.Root>
+			<Popover.Trigger>
+				<Button size="icon" variant="link" title="More reactions">
+					<Ellipsis />
+				</Button>
+			</Popover.Trigger>
+			<Popover.Content>
+				<div
+					class=" z-50 grid grid-cols-6 gap-1.5 p-1.5 bg-white rounded-lg shadow-lg animate-in fade-in slide-in-from-bottom-2"
+				>
+					{#each hiddenEmojis as emoji (emoji)}
+						<Button
+							onclick={() => selectEmoji(emoji)}
+							variant="link"
+							class="hover:scale-125 active:scale-90  rounded-full"
+							title="React with {emoji}"
+						>
+							{emoji}
+						</Button>
+					{/each}
+				</div>
+			</Popover.Content>
+		</Popover.Root>
 	</div>
 
 	<Button onclick={() => handleReply?.(message)} size="icon" variant="link" title="Reply">
